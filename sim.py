5afe7dd32e17506b3c710cd13c0ad141eaefed95
Kandhy-0X9 <kashengkandhy@gmail.com>
2026-04-07 18:40:31 -0400

    Uncomment and organize initial Pygame setup for cannon physics simulation

--- sim.py
+++ sim.py
@@ -1,20 +1,20 @@
-# import pygame
-# import math
+import pygame
+import math
 
-# # Set up display
-# WIDTH, HEIGHT = 1200, 720
-# # Colors
-# BG = ("white")
-# pygame.init() # Initialize Pygame
-# screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# pygame.display.set_caption("Cannon Physics Simulation")
-# running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
+# Set up display
+WIDTH, HEIGHT = 1200, 720
+# Colors
+BG = ("white")
+pygame.init() # Initialize Pygame
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Cannon Physics Simulation")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
     
-#     screen.fill(BG)
-#     pygame.display.flip()
+    screen.fill(BG)
+    pygame.display.flip()
 
-# pygame.quit() # Quit Pygame
+pygame.quit() # Quit Pygame
